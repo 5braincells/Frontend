3ebb3885324e32b742881ef31235e3fd3cf87c8f
YorKnEz <alexmitreanu10@gmail.com>
2021-04-06 21:41:44 +0300

    update chat

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as Icons from '@fortawesome/free-solid-svg-icons'
+
 export default function Home() {
   return (
     <Container className='container'>
-      <h1>Welcome to the Home page!</h1>
+      <h1>
+        Welcome to the <FontAwesomeIcon color='#212529' icon={Icons.faHome} />{' '}
+        page!
+      </h1>
     </Container>
   )
 }
