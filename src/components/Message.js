8850a20dc21d1e689b19e129cb0a643e1dc715e4
YorKnEz <alexmitreanu10@gmail.com>
2021-04-07 19:03:30 +0300

    modal imagini

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,11 +3,13 @@ import jwt_decode from 'jwt-decode'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as Icons from '@fortawesome/free-solid-svg-icons'
+import ImageModal from './ImageModal'
 
 export default function Message({ message }) {
   const jwt = localStorage.getItem('jwt') || sessionStorage.getItem('jwt')
   const jwtDecoded = jwt_decode(jwt)
   const userID = jwtDecoded.userID
+  const [modalShow, setModalShow] = React.useState(false)
 
   return (
     <div
@@ -16,15 +18,25 @@ export default function Message({ message }) {
         userID === message.author ? 'message-sent' : 'message-received'
       }`}>
       {message.type === 'msg' || message.type === undefined ? (
-        <div class="message-text">{message.message}</div>
+        <div class='message-text'>{message.message}</div>
       ) : message.type === 'img' ? (
-        <img
-          className='message-image'
-          src={process.env.REACT_APP_IP_PUBLIC + '/files/' + message.filename}
-          alt={message.filename}
-        />
+        <>
+          <img
+            onClick={() => setModalShow(true)}
+            className='message-image'
+            src={process.env.REACT_APP_IP_PUBLIC + '/files/' + message.filename}
+            alt={message.filename}
+          />
+          <ImageModal
+            show={modalShow}
+            imgSrc={
+              process.env.REACT_APP_IP_PUBLIC + '/files/' + message.filename
+            }
+            onHide={() => setModalShow(false)}
+          />
+        </>
       ) : (
-        <div class="message-file">
+        <div class='message-file'>
           <FontAwesomeIcon
             style={{ marginRight: '4px', height: '1.2rem', width: '1.2rem' }}
             color='#fff'
